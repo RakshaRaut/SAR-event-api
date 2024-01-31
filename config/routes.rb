88c6197133693ef7e9508f57afb18e9891aa33f5
Raksha Raut <raksharaut174@gmail.com>
2024-01-31 17:55:14 +0545

    install devise and devise token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :events
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
