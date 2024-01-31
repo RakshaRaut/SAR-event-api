cb770addbefb46f310e42a518565226cd9777bf4
Raksha Raut <raksharaut174@gmail.com>
2024-01-31 18:45:59 +0545

    reorder the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :attendances
   resources :participants
-  mount_devise_token_auth_for 'User', at: 'auth'
   resources :events
 end
