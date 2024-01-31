3f36b852029fe183da3f8fa22eb4a375e7a6ca1f
Raksha Raut <raksharaut174@gmail.com>
2024-01-31 18:03:00 +0545

    add validation for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  validates :title,
+            :start_date,
+            :coordinator,
+            :sub_coordinator,
+            :description,
+            :venue, presence: true
 end
