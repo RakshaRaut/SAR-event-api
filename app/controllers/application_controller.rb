0924dda2b291aa3f6101ba7d56613fc936ea9da4
Raksha Raut <raksharaut174@gmail.com>
2024-02-03 19:41:45 +0545

    add datatype hash and serialize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  DATA_TYPE = {
+    collection: 'collection',
+    object: 'object'
+  }.with_indifferent_access.freeze
+
+  def serialize(data, serializer, type = DATA_TYPE[:collection])
+    if type.to_s.eql? DATA_TYPE[:object]
+      return ActiveModelSerializers::SerializableResource.new(data, serializer: serializer)
+    end
+
+    ActiveModelSerializers::SerializableResource.new(data, each_serializer: serializer)
+  end
 end
